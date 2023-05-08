a03d70fbb20f5b40defb6562e05abbb24d3a872e
khushbu <khushbusoni475@gmail.com>
2023-05-08 16:52:12 +0530

    [khushbu] Adding HelloWorld project

---
+++ HelloWorld281/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld281
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //object creation of human class 
+            //className objectVarible=new ClassName 
+            Human human = new Human();
+            Console.WriteLine("please enter your name");
+            human.name = Console.ReadLine();
+            Console.WriteLine("please enter your height");
+            string height = Console.ReadLine();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ HelloWorld281/human.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld281
+{
+    //access-modifier datatype className
+    public class Human
+    {
+        //instance variable
+        //access-modifier datatype variableName
+        public string name;
+        public float height;
+        public long phoneNumber;
+
+        //access-modifier returntype methodName
+        public void Talk()
+        {
+            Console.WriteLine("Human can talk");
+        }
+        public void HumanDetails()
+        {
+            Console.WriteLine("name:{0} height:{1} phoneNumber:{2}", name, height, phoneNumber);
+        }
+
+    }
+}
